ecebdf1115e48a799140401f4e992f705475d8d5
Rom Grk <romgrk.cc@gmail.com>
2017-09-19 22:09:38 -0400

    add fzfdefaults

--- plugins/fzf.zsh
+++ plugins/fzf.zsh
@@ -18,3 +18,20 @@ fi
 # FIXME for colors
 [[ ! ( $TERM =~ '256' ) ]] && export TERM="xterm-256color"
 
+
+export FZF_DEFAULT_OPTS='--inline-info
+--margin 0,0,0,2
+--ansi
+--select-1
+--color dark
+--color fg+:253,bg+:236,hl+:196,pointer:196
+--color hl:164,info:202,prompt:196,header:190
+--bind alt-j:down,alt-k:up,tab:up
+--bind alt-u:page-up,alt-d:page-down
+--bind ctrl-u:kill-line,ctrl-d:page-down
+--bind alt-s:toggle-sort
+--bind alt-i:ignore
+--bind ";:execute(less {})"
+--bind "alt-v:execute(gloobus-preview {})"
+--bind "alt-y:execute(echo {} | xclip -selection clipboard)"
+'
