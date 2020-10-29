545ea2c8498a6013f5b160e2f30e6602c3a0f7fe
Rom Grk <romgrk.cc@gmail.com>
2020-10-29 19:40:04 -0400

    highlights: make existing file more readable

--- highlight.zsh
+++ highlight.zsh
@@ -23,7 +23,7 @@ local blue=39
 # FAST_HIGHLIGHT_STYLES[hashed-command]='fg=green'
 # To have paths colored instead of underlined
 FAST_HIGHLIGHT_STYLES[path]='fg=magenta'
-FAST_HIGHLIGHT_STYLES[path-to-dir]='fg=magenta'
+FAST_HIGHLIGHT_STYLES[path-to-dir]='fg=#B460FF'
 FAST_HIGHLIGHT_STYLES[path_pathseparator]='fg=#8700ff'
 FAST_HIGHLIGHT_STYLES[globbing]="fg=$blue"
 # FAST_HIGHLIGHT_STYLES[globbing-ext]='fg=13'
